cd09038b689da76700f0b5f3239187a8084de9c6
jisobaek <jisobaek@f6r6s12.paris.42.school>
2026-09-06 11:49:41 +0200

    e

---
+++ ex0/construct.py
@@ -0,0 +1,33 @@
+import sys
+import os
+env_name = os.path.basename(sys.prefix)
+package_path = os.path.join(sys.prefix, "lib", "site-packages")
+
+if sys.prefix != sys.base_prefix:
+     print("MATRIX STATUS: Welcome to the construct")
+     print()
+     print(f"Current Python: {sys.executable}")
+     print(f"Virtual Environment: {env_name}")
+     print(f"Environment Path: {sys.prefix}")
+     print()
+     print("SUCCESS: You’re in an isolated environment!")
+     print("Safe to install packages without affecting")
+     print("the global system.")
+     print()
+     print("Package installation path:")
+     print(f"{package_path}")
+else:
+    print("MATRIX STATUS: You’re still plugged in")
+    print()
+    print(f"Current Python: {sys.executable}")
+    print("Virtual Environment: None detected")
+    print()
+    print("WARNING: You’re in the global environment!")
+    print("The machines can see everything you install.")
+    print()
+    print("To enter the construct, run:")
+    print("python -m venv matrix_env")
+    print("source matrix_env/bin/activate # On Unix")
+    print("matrix_env\Scripts\activate # On Windows")
+    print()
+    print("Then run this program again.")
